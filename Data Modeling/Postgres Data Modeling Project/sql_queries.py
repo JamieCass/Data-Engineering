b28af7dc55ce81adcae0c39062ec8632eda2f256
JamieCass <jamiecass92@gmail.com>
2020-12-01 17:06:21 +1000

    added more to the project folder and added a log-data picture

--- Data Modeling/Postgres Data Modeling Project/sql_queries.py
+++ Data Modeling/Postgres Data Modeling Project/sql_queries.py
@@ -1,26 +1,37 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS songplays (songplay_id int, start_time numeric,
+           														  user_id int, level varchar, 
+           														  song_id varchar, artist_id varchar, 
+           				 										  session_id int, location varchar,
+           				 										  user_agent varchar)
 """)
 
-user_table_create = ("""
+user_table_create = ("""CREATE TABLE IF NOT EXISTS users (user_id varchar, first_name varchar, 
+       				 									  last_name varchar, gender varchar, 
+      				 									  level varchar)
 """)
 
-song_table_create = ("""
+song_table_create = ("""CREATE TABLE IF NOT EXISTS songs (song_id varchar, title varchar, 
+      													  artist_id varchar, year int, duration numeric)
 """)
 
-artist_table_create = ("""
+artist_table_create = ("""CREATE TABLE IF NOT EXISTS artists (artist_id varchar, name varchar,
+        													  location varchar, latitude numeric,
+         													  longitude numeric)
 """)
 
-time_table_create = ("""
+time_table_create = ("""CREATE TABLE IF NOT EXISTS time (start_time numeric, hour int,
+        												 day int, week int, month int, 
+        												 year int, weekday int)
 """)
 
 # INSERT RECORDS
@@ -31,7 +42,8 @@ songplay_table_insert = ("""
 user_table_insert = ("""
 """)
 
-song_table_insert = ("""
+song_table_insert = ("""INSERT INTO songs (song_id, title, artist_id, year, duration),
+VALUES(%s, %s, %s, %s, %s)
 """)
 
 artist_table_insert = ("""
