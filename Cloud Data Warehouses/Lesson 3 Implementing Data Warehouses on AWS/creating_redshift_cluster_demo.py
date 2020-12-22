46a6093e9b7bc04a90394312d595d3dc3c3d4f92
JamieCass <jamiecass92@gmail.com>
2020-12-22 11:24:42 +1000

    added a demo of IaC creating a redshift cluster and iamrole

---
+++ Cloud Data Warehouses/Lesson 3 Implementing Data Warehouses on AWS/creating_redshift_cluster_demo.py
@@ -0,0 +1,61 @@
+##########################################
+# Create Redshift cluster
+##########################################
+
+redshift = boto3.client('redshift',
+						region_name='us-west-2',
+						aws_access_key_id=KEY,
+						aws_secret_access_key=SECRET
+						)
+
+												response = redshift.create.cluster(
+													# HW
+													ClusterType=DWH_CLUSTER_TYPE,
+													NodeType=DWH_NODE_TYPE,
+													NumberOfNodes=int(DWH_NUM_NODES),
+
+													# Identifiers & Credentials
+													DBName=DWH_DB,
+													ClusterIdentifier=DWH_CLUSTER_IDENTIFIER,
+													MasterUsername=DWH_DB_USER,
+													MasterUserPassword=DWH_USER_PASSWORD
+
+													# Roles (for S3 access)
+													IamRoles=[roleArn]
+												)
+
+##########################################
+# Create new IAM role and Attaching Policy
+##########################################
+	dwhRole = iam.create_role(
+		Path='/',
+		RoleName=DWH_ROLE_NAME,
+		Description='Allows Redshift cluster to call AWS services on your behalf',
+		AssumeRolePolicyDocuments=json.dumps(
+			{'Statment' : [{'Action': 'sts:AssumeRole',
+				'Effect' : 'Allow',
+				'Principle' : {'Service' : 'redshift.amazonaws.com'}}],
+			 'Version' : '2012-10-17'})
+	)
+
+	iam.attach_role_policy(RoleName=DWH_IAM_ROLE_NAME,
+		PolicyArn='arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess'
+		)['ResponseMetadata']['HTTPStatusCode']
+
+
+##########################################
+# E.g. Open an incoming TCP port to access the cluster endpoint
+##########################################
+	vpc = ec2.Vpc(id=myClusterProps['VpcId'])
+	defaultSg = list(vpc.security_groups.all())[0]
+	defaultSg.authorize_ingress(
+		GroupName=defaultSg.group_name,
+		CidrIp='0.0.0.0/0',
+		IpProtocol='TCP',
+		FromPort=int(DWH_PORT),
+		ToPort=int(DWH_PORT)
+	)
+
+
+
+
