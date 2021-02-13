15b8f0ac107835ed3c7843740d2a4f38d8dedd74
Jamie Cass <jamiecass92@gmail.com>
2021-02-13 22:49:27 +1000

    played around with skewness exercise, not done too much on it though

--- Data Lakes with Spark/Lesson 4 Debugging & Optimization/skewness_intro_exercise.py
+++ Data Lakes with Spark/Lesson 4 Debugging & Optimization/skewness_intro_exercise.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import desc 
 
 def explore_dataframe():
 	
 	spark = SparkSession.builder.appName("Skewness Introduction").getOrCreate()
 
 	#TODO get your file path from s3 for parking_violation.csv
-	input_path = ""
+	input_path = "/Users/jamie/data/parking_violation.csv"
 	df = spark.read.format("csv").option("header", True).load(input_path)
 
 	# investigate what columns you have
@@ -13,11 +14,15 @@ def explore_dataframe():
 	print(col_list)
 
     # TODO groupby month and year to get count
-	year_df = df.groupby("")
-    month_df = df.groupby("")
+	year_df = df.groupby("year")
+	month_df = df.groupby("month")
+
+	year_df.count().orderBy(desc('count')).show()
+	month_df.count().orderBy(desc('count')).show()
 
     # TODO write file partition by year, and study the executor in the spark UI
     # TODO use repartition function
+	df.write.partitionBy('year').csv('trial-year')
 
 
 if __name__ == "__main__":
