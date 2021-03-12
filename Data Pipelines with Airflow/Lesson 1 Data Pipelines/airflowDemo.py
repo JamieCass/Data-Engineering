064a42c1c06fb4dc50b29eff20fc59b0d0fd8193
Jamie <jamiecass92@gmail.com>
2021-03-12 15:39:28 +1000

    finished lesson 1, started lesson 2 data quality

--- Data Pipelines with Airflow/Lesson 1 Data Pipelines/airflowDemo.py
+++ Data Pipelines with Airflow/Lesson 1 Data Pipelines/airflowDemo.py
@@ -184,6 +184,106 @@ list_task = PythonOperator(
 
 
 
+##################### Runtime Variables ##################### 
+# Instructions
+# Use the Airflow context in the pythonoperator to complete the TODOs below. Once you are done, run your DAG and check the logs to see the context in use.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+
+
+def log_details(*args, **kwargs):
+    #
+    # TODO: Extract ds, run_id, prev_ds, and next_ds from the kwargs, and log them
+    # NOTE: Look here for context variables passed in on kwargs:
+    #       https://airflow.apache.org/macros.html
+    #
+    ds = {kwargs['ds']}
+    run_id = {kwargs['run_id']}
+    previous_ds = {kwargs.get('prev_ds')}
+    next_ds = {kwargs.get('next_ds')}
+
+    logging.info(f"Execution date is {ds}")
+    logging.info(f"My run id is {run_id}")
+    if previous_ds:
+        logging.info(f"My previous run was on {previous_ds}")
+    if next_ds:
+        logging.info(f"My next run will be {next_ds}")
+
+dag = DAG(
+    'lesson1.exercise5',
+    schedule_interval="@daily",
+    start_date=datetime.datetime.now() - datetime.timedelta(days=2)
+)
+
+list_task = PythonOperator(
+    task_id="log_details",
+    python_callable=log_details,
+    provide_context=True,
+    dag=dag
+)
+
+
+
+
+##################### S3 to Redshift Exercise #####################
+# Instructions
+# Similar to what you saw in the demo, copy and populate the trips table. Then, add another operator which creates a traffic analysis table from the trips table you created. Note, in this class, we won’t be writing SQL -- all of the SQL statements we run against Redshift are predefined and included in your lesson.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    redshift_hook.run(sql_statements.COPY_ALL_TRIPS_SQL.format(credentials.access_key, credentials.secret_key))
+
+
+dag = DAG(
+    'lesson1.exercise6',
+    start_date=datetime.datetime.now()
+)
+
+create_table = PostgresOperator(
+    task_id="create_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_task = PythonOperator(
+    task_id='load_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_data_to_redshift
+)
+
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.LOCATION_TRAFFIC_SQL
+)
+
+create_table >> copy_task
+copy_task >> location_traffic_task
+
+
+
 
 
 
