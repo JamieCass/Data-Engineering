8b020f182466cf18d0d3f427cc9420cec0bbd0f9
Jamie <>
2021-04-06 15:21:00 +1000

    seperated the lesson exercises into seperate files for each lesson

---
+++ Data Pipelines with Airflow/Lesson 1 Data Pipelines/lesson1_exercises.py
@@ -0,0 +1,288 @@
+####################################################################
+# LESSON 1
+####################################################################
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+# To start airflow on course
+/opt/airflow/start.sh
+
+# TODO: Define a function for the PythonOperator to call
+
+def my_function():
+	logging.info('Hello world!!')
+
+dag = DAG(
+		'lesson1.demo',
+		start_date=datetime.datetime.now())
+
+
+
+greet_task = PythonOperator(
+	task_id='test',
+	python_callable=my_function, # this line calls the functions through the python operator
+	dag=dag
+	)
+
+
+##################### Data Pipeline Example #####################
+# Create a DAG with a start date and interval
+divvy_day = DAG(
+	'divvy',
+	description='Analyzes Divvy Bikeshare data',
+	start_date=datetime.datetime(2019, 2, 4),
+	schedule_interval='@daily'
+	)
+
+# Create a function for the DAG to run
+def hello_world():
+	print('Hello World!!')
+
+# Create the task that the DAG will run (calling the function to run)
+task = PythonOperator(
+	task_id='hello_world',
+	python_callable=hello_world,
+	dag=divvy_dag)
+
+
+##################### Task Dependencies #####################
+
+a >> b # means a comes before b
+a << b # means a comes after b
+
+hello_world_task=PythonOperator(task_id='hello_world', ...)
+goodbye_world_task=PythonOperator(task_id='goodbye_world', ...)
+...
+# Use >> to denote that goodbye_world_task depends on hello_world_task
+hello_world_task >> goodbye_world_task
+
+# OR
+a.set_downstream(b) # means a comes before b
+a.set_upstream(b) # means a comes after b
+hello_world_task.set_downstream(goodbye_world_task)
+
+
+##################### Exercise 3 #####################
+def hello_world():
+    logging.info("Hello World")
+
+
+def addition():
+    logging.info(f"2 + 2 = {2+2}")
+
+
+def subtraction():
+    logging.info(f"6 -2 = {6-2}")
+
+
+def division():
+    logging.info(f"10 / 2 = {int(10/2)}")
+
+
+dag = DAG(
+    "lesson1.exercise3",
+    schedule_interval='@hourly',
+    start_date=datetime.datetime.now() - datetime.timedelta(days=1))
+
+hello_world_task = PythonOperator(
+    task_id="hello_world",
+    python_callable=hello_world,
+    dag=dag)
+
+#
+# TODO: Define an addition task that calls the `addition` function above
+addition_task = PythonOperator(
+    task_id='addition_task',
+    python_callable=addition,
+    dag=dag)
+
+#
+# TODO: Define a subtraction task that calls the `subtraction` function above
+subtraction_task = PythonOperator(
+    task_id='subtraction_task',
+    python_callable=subtraction,
+    dag=dag)
+
+#
+# TODO: Define a division task that calls the `division` function above
+division_task = PythonOperator(
+    task_id='division_task',
+    python_callable=division,
+    dag=dag)
+
+#
+# TODO: Configure the task dependencies such that the graph looks like the following:
+#
+#                    ->  addition_task
+#                   /                 \
+#   hello_world_task                   -> division_task
+#                   \                 /
+#                    ->subtraction_task
+hello_world_task >> addition_task
+hello_world_task >> subtraction_task
+addition_task >> division_task
+subtraction_task >> division_task
+
+
+
+##################### Hooks #####################
+from airflow import DAG
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.python_operator import PythonOperator
+
+def load():
+# Create a PostgresHook option using the `demo` connection
+    db_hook = PostgresHook(‘demo’)
+    df = db_hook.get_pandas_df('SELECT * FROM rides')
+    print(f'Successfully used PostgresHook to return {len(df)} records')
+
+load_task = PythonOperator(task_id=’load’, python_callable=hello_world, ...)
+
+
+##################### Connections #####################
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+
+#
+# TODO: There is no code to modify in this exercise. We're going to create a connection and a
+# variable.
+# 1. Open your browser to localhost:8080 and open Admin->Variables
+# 2. Click "Create"
+# 3. Set "Key" equal to "s3_bucket" and set "Val" equal to "udacity-dend"
+# 4. Set "Key" equal to "s3_prefix" and set "Val" equal to "data-pipelines"
+# 5. Click save
+# 6. Open Admin->Connections
+# 7. Click "Create"
+# 8. Set "Conn Id" to "aws_credentials", "Conn Type" to "Amazon Web Services"
+# 9. Set "Login" to your aws_access_key_id and "Password" to your aws_secret_key
+# 10. Click save
+# 11. Run the DAG
+
+def list_keys():
+    hook = S3Hook(aws_conn_id='aws_credentials')
+    bucket = Variable.get('s3_bucket')
+    prefix = Variable.get('s3_prefix')
+    logging.info(f"Listing Keys from {bucket}/{prefix}")
+    keys = hook.list_keys(bucket, prefix=prefix)
+    for key in keys:
+        logging.info(f"- s3://{bucket}/{key}")
+
+
+dag = DAG(
+        'lesson1.exercise4',
+        start_date=datetime.datetime.now())
+
+list_task = PythonOperator(
+    task_id="list_keys",
+    python_callable=list_keys,
+    dag=dag
+)
+
+
+
+##################### Runtime Variables #####################
+# Instructions
+# Use the Airflow context in the pythonoperator to complete the TODOs below. Once you are done, run your DAG and check the logs to see the context in use.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.S3_hook import S3Hook
+
+
+def log_details(*args, **kwargs):
+    #
+    # TODO: Extract ds, run_id, prev_ds, and next_ds from the kwargs, and log them
+    # NOTE: Look here for context variables passed in on kwargs:
+    #       https://airflow.apache.org/macros.html
+    #
+    ds = {kwargs['ds']}
+    run_id = {kwargs['run_id']}
+    previous_ds = {kwargs.get('prev_ds')}
+    next_ds = {kwargs.get('next_ds')}
+
+    logging.info(f"Execution date is {ds}")
+    logging.info(f"My run id is {run_id}")
+    if previous_ds:
+        logging.info(f"My previous run was on {previous_ds}")
+    if next_ds:
+        logging.info(f"My next run will be {next_ds}")
+
+dag = DAG(
+    'lesson1.exercise5',
+    schedule_interval="@daily",
+    start_date=datetime.datetime.now() - datetime.timedelta(days=2)
+)
+
+list_task = PythonOperator(
+    task_id="log_details",
+    python_callable=log_details,
+    provide_context=True,
+    dag=dag
+)
+
+
+
+
+##################### S3 to Redshift Exercise #####################
+# Instructions
+# Similar to what you saw in the demo, copy and populate the trips table. Then, add another operator which creates a traffic analysis table from the trips table you created. Note, in this class, we won’t be writing SQL -- all of the SQL statements we run against Redshift are predefined and included in your lesson.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    redshift_hook.run(sql_statements.COPY_ALL_TRIPS_SQL.format(credentials.access_key, credentials.secret_key))
+
+
+dag = DAG(
+    'lesson1.exercise6',
+    start_date=datetime.datetime.now()
+)
+
+create_table = PostgresOperator(
+    task_id="create_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_task = PythonOperator(
+    task_id='load_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_data_to_redshift
+)
+
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.LOCATION_TRAFFIC_SQL
+)
+
+create_table >> copy_task
+copy_task >> location_traffic_task
---
+++ Data Pipelines with Airflow/Lesson 2 Data Quality/lesson2_exercises.py
@@ -0,0 +1,383 @@
+####################################################################
+# LESSON 2
+####################################################################
+##################### Exercise 1 #####################
+#Instructions
+#1 - Run the DAG as it is first, and observe the Airflow UI
+#2 - Next, open up the DAG and add the copy and load tasks as directed in the TODOs
+#3 - Reload the Airflow UI and run the DAG once more, observing the Airflow UI
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_trip_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.COPY_ALL_TRIPS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+def load_station_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.COPY_STATIONS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+dag = DAG(
+    'lesson2.exercise1',
+    start_date=datetime.datetime.now()
+)
+
+create_trips_table = PostgresOperator(
+    task_id="create_trips_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_trips_task = PythonOperator(
+    task_id='load_trips_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_trip_data_to_redshift,
+)
+
+create_stations_table = PostgresOperator(
+    task_id="create_stations_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_STATIONS_TABLE_SQL,
+)
+
+copy_stations_task = PythonOperator(
+    task_id='load_stations_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_station_data_to_redshift,
+)
+
+create_trips_table >> copy_trips_task
+create_stations_table >> copy_stations_task
+# TODO: First, load the Airflow UI and run this DAG once.
+# TODO: Next, configure the task ordering for stations data to have the create run before
+#       the copy. Then, run this DAG once more and inspect the run history. to see the
+#       differences.
+
+
+##################### Exercise 2 #####################
+
+#Instructions
+#1 - Revisit our bikeshare traffic
+#2 - Update our DAG with
+#  a - @monthly schedule_interval
+#  b - max_active_runs of 1
+#  c - start_date of 2018/01/01
+#  d - end_date of 2018/02/01
+# Use Airflow’s backfill capabilities to analyze our trip data on a monthly basis over 2 historical runs
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_trip_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.COPY_ALL_TRIPS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+def load_station_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.COPY_STATIONS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+dag = DAG(
+    'lesson2.exercise2',
+    start_date=datetime.datetime(2018, 1, 1, 0, 0, 0, 0),
+    # TODO: Set the end date to February first
+    end_date=datetime.datetime(2018, 2, 1, 0, 0, 0, 0),
+    # TODO: Set the schedule to be monthly
+    schedule_interval='@monthly',
+    # TODO: set the number of max active runs to 1
+    max_active_runs=1
+)
+
+create_trips_table = PostgresOperator(
+    task_id="create_trips_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_trips_task = PythonOperator(
+    task_id='load_trips_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_trip_data_to_redshift,
+    provide_context=True,
+)
+
+create_stations_table = PostgresOperator(
+    task_id="create_stations_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_STATIONS_TABLE_SQL,
+)
+
+copy_stations_task = PythonOperator(
+    task_id='load_stations_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_station_data_to_redshift,
+)
+
+create_trips_table >> copy_trips_task
+create_stations_table >> copy_stations_task
+
+
+##################### Exercise 3 #####################
+
+#Instructions
+#1 - Modify the bikeshare DAG to load data month by month, instead of loading it all at once, every time.
+#2 - Use time partitioning to parallelize the execution of the DAG.
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_trip_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+
+    # # #
+    # TODO: How do we get the execution_date from our context?
+    execution_date=kwargs["execution_date"]
+    # execution_date = datetime.datetime.utcnow()
+    # # #
+
+    sql_stmt = sql_statements.COPY_MONTHLY_TRIPS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+        year=execution_date.year,
+        month=execution_date.month
+    )
+    redshift_hook.run(sql_stmt)
+
+
+def load_station_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.COPY_STATIONS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+dag = DAG(
+    'lesson2.exercise3',
+    start_date=datetime.datetime(2018, 1, 1, 0, 0, 0, 0),
+    end_date=datetime.datetime(2019, 1, 1, 0, 0, 0, 0),
+    schedule_interval='@monthly',
+    max_active_runs=1
+)
+
+create_trips_table = PostgresOperator(
+    task_id="create_trips_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_trips_task = PythonOperator(
+    task_id='load_trips_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_trip_data_to_redshift,
+    # TODO: ensure that we provide context to our Python Operator
+    provide_context=True
+)
+
+create_stations_table = PostgresOperator(
+    task_id="create_stations_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_STATIONS_TABLE_SQL,
+)
+
+copy_stations_task = PythonOperator(
+    task_id='load_stations_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_station_data_to_redshift,
+)
+
+create_trips_table >> copy_trips_task
+create_stations_table >> copy_stations_task
+
+
+
+##################### Exercise 4 #####################
+#Instructions
+#1 - Set an SLA on our bikeshare traffic calculation operator
+#2 - Add data verification step after the load step from s3 to redshift
+#3 - Add data verification step after we calculate our output table
+
+import datetime
+import logging
+
+from airflow import DAG
+from airflow.contrib.hooks.aws_hook import AwsHook
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.python_operator import PythonOperator
+
+import sql_statements
+
+
+def load_trip_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    execution_date = kwargs["execution_date"]
+    sql_stmt = sql_statements.COPY_MONTHLY_TRIPS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+        year=execution_date.year,
+        month=execution_date.month
+    )
+    redshift_hook.run(sql_stmt)
+
+
+def load_station_data_to_redshift(*args, **kwargs):
+    aws_hook = AwsHook("aws_credentials")
+    credentials = aws_hook.get_credentials()
+    redshift_hook = PostgresHook("redshift")
+    sql_stmt = sql_statements.COPY_STATIONS_SQL.format(
+        credentials.access_key,
+        credentials.secret_key,
+    )
+    redshift_hook.run(sql_stmt)
+
+
+def check_greater_than_zero(*args, **kwargs):
+    table = kwargs["params"]["table"]
+    redshift_hook = PostgresHook("redshift")
+    records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
+    if len(records) < 1 or len(records[0]) < 1:
+        raise ValueError(f"Data quality check failed. {table} returned no results")
+    num_records = records[0][0]
+
+    #
+    # TODO: Add a check here to verify that at least one record was found
+    #       Raise an error if less than one record is found
+    #
+    if records is None or len(records[0]) < 1:
+        logging.error(f'No records found in table {table}')
+        raise ValueError(f'No records found in table {table}')
+
+    logging.info(f"Data quality on table {table} check passed with {records[0][0]} records")
+
+
+dag = DAG(
+    'lesson2.exercise4',
+    start_date=datetime.datetime(2018, 1, 1, 0, 0, 0, 0),
+    end_date=datetime.datetime(2019, 1, 1, 0, 0, 0, 0),
+    schedule_interval='@monthly',
+    max_active_runs=1
+)
+
+create_trips_table = PostgresOperator(
+    task_id="create_trips_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_TRIPS_TABLE_SQL
+)
+
+copy_trips_task = PythonOperator(
+    task_id='load_trips_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_trip_data_to_redshift,
+    provide_context=True,
+)
+
+check_trips = PythonOperator(
+    task_id='check_trips_data',
+    dag=dag,
+    python_callable=check_greater_than_zero,
+    provide_context=True,
+    params={
+        'table': 'trips',
+    }
+)
+
+create_stations_table = PostgresOperator(
+    task_id="create_stations_table",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.CREATE_STATIONS_TABLE_SQL,
+)
+
+copy_stations_task = PythonOperator(
+    task_id='load_stations_from_s3_to_redshift',
+    dag=dag,
+    python_callable=load_station_data_to_redshift,
+)
+
+check_stations = PythonOperator(
+    task_id='check_stations_data',
+    dag=dag,
+    python_callable=check_greater_than_zero,
+    provide_context=True,
+    params={
+        'table': 'stations',
+    }
+)
+
+create_trips_table >> copy_trips_task
+create_stations_table >> copy_stations_task
+
+#
+# TODO: Set the task dependencies for the stations and trips check tasks
+copy_trips_task >> check_trips
+copy_stations_task >> check_stations
