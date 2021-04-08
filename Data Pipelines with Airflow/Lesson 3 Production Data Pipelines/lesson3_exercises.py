595e3f32ace00fc68e83a2c86ede7d555e6531dc
Jamie <jamiecass92@gmail.com>
2021-04-08 15:30:01 +1000

    added a few more exercises to lesson 3

--- Data Pipelines with Airflow/Lesson 3 Production Data Pipelines/lesson3_exercises.py
+++ Data Pipelines with Airflow/Lesson 3 Production Data Pipelines/lesson3_exercises.py
@@ -224,7 +224,161 @@ create_oldest_task >> log_oldest_task
 
 
 
+##################### Exercise 3 #####################
+
+##### DAG 
+#Instructions
+#In this exercise, we’ll place our S3 to RedShift Copy operations into a SubDag.
+#1 - Consolidate HasRowsOperator into the SubDag
+#2 - Reorder the tasks to take advantage of the SubDag Operators
+
+import datetime
+
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.operators.udacity_plugin import HasRowsOperator
+
+from lesson3.exercise3.subdag import get_s3_to_redshift_dag
+import sql_statements
 
 
+start_date = datetime.datetime.utcnow()
+
+dag = DAG(
+    "lesson3.exercise3",
+    start_date=start_date,
+)
+
+trips_task_id = "trips_subdag"
+trips_subdag_task = SubDagOperator(
+    subdag=get_s3_to_redshift_dag(
+        "lesson3.exercise3",
+        trips_task_id,
+        "redshift",
+        "aws_credentials",
+        "trips",
+        sql_statements.CREATE_TRIPS_TABLE_SQL,
+        s3_bucket="udac-data-pipelines",
+        s3_key="divvy/unpartitioned/divvy_trips_2018.csv",
+        start_date=start_date,
+    ),
+    task_id=trips_task_id,
+    dag=dag,
+)
+
+stations_task_id = "stations_subdag"
+stations_subdag_task = SubDagOperator(
+    subdag=get_s3_to_redshift_dag(
+        "lesson3.exercise3",
+        stations_task_id,
+        "redshift",
+        "aws_credentials",
+        "stations",
+        sql_statements.CREATE_STATIONS_TABLE_SQL,
+        s3_bucket="udac-data-pipelines",
+        s3_key="divvy/unpartitioned/divvy_stations_2017.csv",
+        start_date=start_date,
+    ),
+    task_id=stations_task_id,
+    dag=dag,
+)
+
+#
+# TODO: Consolidate check_trips and check_stations into a single check in the subdag
+#       as we did with the create and copy in the demo
+#
+check_trips = HasRowsOperator(
+    task_id="check_trips_data",
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="trips"
+)
+
+check_stations = HasRowsOperator(
+    task_id="check_stations_data",
+    dag=dag,
+    redshift_conn_id="redshift",
+    table="stations"
+)
+
+location_traffic_task = PostgresOperator(
+    task_id="calculate_location_traffic",
+    dag=dag,
+    postgres_conn_id="redshift",
+    sql=sql_statements.LOCATION_TRAFFIC_SQL
+)
+
+#
+# TODO: Reorder the Graph once you have moved the checks
+#
+trips_subdag_task >> check_trips
+stations_subdag_task >> check_stations
+check_stations >> location_traffic_task
+check_trips >> location_traffic_task
+
+
+##### SUBDAG 
+#Instructions
+#In this exercise, we’ll place our S3 to RedShift Copy operations into a SubDag.
+#1 - Consolidate HasRowsOperator into the SubDag
+#2 - Reorder the tasks to take advantage of the SubDag Operators
+
+import datetime
+
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.operators.udacity_plugin import HasRowsOperator
+from airflow.operators.udacity_plugin import S3ToRedshiftOperator
+
+import sql
+
+
+# Returns a DAG which creates a table if it does not exist, and then proceeds
+# to load data into that table from S3. When the load is complete, a data
+# quality  check is performed to assert that at least one row of data is
+# present.
+def get_s3_to_redshift_dag(
+        parent_dag_name,
+        task_id,
+        redshift_conn_id,
+        aws_credentials_id,
+        table,
+        create_sql_stmt,
+        s3_bucket,
+        s3_key,
+        *args, **kwargs):
+    dag = DAG(
+        f"{parent_dag_name}.{task_id}",
+        **kwargs
+    )
+
+    create_task = PostgresOperator(
+        task_id=f"create_{table}_table",
+        dag=dag,
+        postgres_conn_id=redshift_conn_id,
+        sql=create_sql_stmt
+    )
+
+    copy_task = S3ToRedshiftOperator(
+        task_id=f"load_{table}_from_s3_to_redshift",
+        dag=dag,
+        table=table,
+        redshift_conn_id=redshift_conn_id,
+        aws_credentials_id=aws_credentials_id,
+        s3_bucket=s3_bucket,
+        s3_key=s3_key
+    )
+
+    #
+    # TODO: Move the HasRowsOperator task here from the DAG
+    #
+
+    create_task >> copy_task
+    #
+    # TODO: Use DAG ordering to place the check task
+    #
+
+    return dag
 
 
